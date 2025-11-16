ec186a829475880a024b9b1e801ec41fe0806512
Biniyam-Belay <binibelaygom@gmail.com>
2025-11-16 16:21:38 +0300

    image quality optimization

--- components/content/ProjectCard.tsx
+++ components/content/ProjectCard.tsx
@@ -69,6 +69,7 @@ export default function ProjectCard({
         src={project.thumb}
         alt={project.alt ?? project.title}
         fill
+        quality={90}
         className="object-cover transition-transform duration-300 group-hover:scale-105"
         sizes="(min-width: 1024px) 33vw, (min-width: 640px) 50vw, 100vw"
       />
